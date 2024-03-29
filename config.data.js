72427d39f43749dcd41266209739a95f59bc9c47
Ebyynew <165349518+Ebyynew@users.noreply.github.com>
2024-03-29 14:35:31 +0700

    Update and rename config.js to config.data.js
    Y

--- config.data.js
+++ config.data.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6282132537743'],
+  ['6282132537743'],
+  ['6282132537743', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
